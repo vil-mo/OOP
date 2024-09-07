91bc16a17e09f89537d68c0e185f4965439e8a58
vil'mo <morgraphvillain@gmail.com>
2024-09-07 22:45:02 +0500

    Moved BinaryHeap to be local class

--- Task_1_1_1/src/main/java/ru/nsu/berezin/Heapsort.java
+++ Task_1_1_1/src/main/java/ru/nsu/berezin/Heapsort.java
@@ -4,15 +4,86 @@ package ru.nsu.berezin;
  * Class that has method for heapsort.
  */
 public class Heapsort {
-    private Heapsort() {}
+
+    private Heapsort() {
+    }
 
     /**
-     * Heapsort uisng `BinaryHeap`.
+     * Heapsort uisng min binary heap.
      * 
      * @param array array that is being sorted. Mutates the passed array
      */
     public static void heapsort(int[] array) {
-        BinaryHeap heap = new BinaryHeap(array.length);
+        class MinBinaryHeap {
+            int[] buf;
+            int size;
+
+            public MinBinaryHeap(int capasity) {
+                buf = new int[capasity];
+            }
+
+            private int parent(int i) {
+                return (i - 1) / 2;
+            }
+
+            private int left(int i) {
+                return 2 * i + 1;
+            }
+
+            private int right(int i) {
+                return 2 * i + 2;
+            }
+
+            public void add(int item) {
+                int i = size;
+                size += 1;
+
+                while (i != 0 && item < buf[parent(i)]) {
+                    buf[i] = buf[parent(i)];
+                    i = parent(i);
+                }
+
+                buf[i] = item;
+            }
+
+            private void restore(int i) {
+                int l = left(i);
+                int r = right(i);
+
+                int min = i;
+
+                if (l < size && buf[l] < buf[min]) {
+                    min = l;
+                }
+                if (r < size && buf[r] < buf[min]) {
+                    min = r;
+                }
+
+                if (min != i) {
+                    int temp = buf[min];
+                    buf[min] = buf[i];
+                    buf[i] = temp;
+                    restore(min);
+                }
+            }
+
+            public int remove() {
+                if (size == 1) {
+                    size--;
+                    return buf[0];
+                }
+
+                int root = buf[0];
+
+                buf[0] = buf[size - 1];
+                size--;
+                restore(0);
+
+                return root;
+            }
+        }
+
+        MinBinaryHeap heap = new MinBinaryHeap(array.length);
         for (int x : array) {
             heap.add(x);
         }
@@ -24,7 +95,9 @@ public class Heapsort {
 
     /**
      * Made to be able to run script
+     * 
      * @param args main args
      */
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+    }
 }
