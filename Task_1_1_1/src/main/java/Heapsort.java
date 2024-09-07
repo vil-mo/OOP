b268baf17268494cd150e9355080a5a283984abf
vil'mo <morgraphvillain@gmail.com>
2024-09-07 19:06:06 +0500

    implemented using PriorityQueue

--- Task_1_1_1/src/main/java/Heapsort.java
+++ Task_1_1_1/src/main/java/Heapsort.java
@@ -1,5 +1,14 @@
+import java.util.PriorityQueue;
+
 public class Heapsort {
     public static void heapsort(int[] array) {
-        
-    }   
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+        for (int x : array) {
+            queue.add(x);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = queue.remove();
+        }
+    }
 }
--- Task_1_1_1/src/test/java/Tests.java
+++ Task_1_1_1/src/test/java/Tests.java
@@ -2,23 +2,25 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
-
 public class Tests {
     @Test
-    void first() {
-        int[] my_arr = new int[] {4, 1, 5};
-
+    void small_array() {
+        int[] my_arr = new int[] { 4, 1, 5 };
         Heapsort.heapsort(my_arr);
-
-        assertArrayEquals(my_arr, new int[] {1, 4, 5});
+        assertArrayEquals(my_arr, new int[] { 1, 4, 5 });
     }
 
     @Test
-    void second() {
-        int[] my_arr = new int[] {4, 3, 1, 5};
-
+    void big_array() {
+        int[] my_arr = new int[] { 4, 45, 3, 1, 5, 10, 18, 6, 19, 5 };
         Heapsort.heapsort(my_arr);
+        assertArrayEquals(my_arr, new int[] { 1, 3, 4, 5, 5, 6, 10, 18, 19, 45 });
+    }
 
-        assertArrayEquals(my_arr, new int[] {1, 3, 4, 5});
+    @Test
+    void negative() {
+        int[] my_arr = new int[] { -4, -4, 5, -4, -1, -6, 0 };
+        Heapsort.heapsort(my_arr);
+        assertArrayEquals(my_arr, new int[] { -6, -4, -4, -4, -1, 0, 5 });
     }
 }
