0618a473be4a3cd9e3d05324c3964375b34cb323
vil'mo <morgraphvillain@gmail.com>
2024-09-10 15:49:23 +0700

    Removed OOP

--- Task_1_1_1/src/main/java/ru/nsu/berezin/Heapsort.java
+++ Task_1_1_1/src/main/java/ru/nsu/berezin/Heapsort.java
@@ -8,94 +8,77 @@ public class Heapsort {
     private Heapsort() {
     }
 
-    /**
-     * Heapsort uisng min binary heap.
-     * 
-     * @param array - Array that is being sorted. Mutates the passed array.
-     */
-    public static void heapsort(int[] array) {
-        class MinBinaryHeap {
-            int[] buf;
-            int size;
+    static private int parent(int i) {
+        return (i - 1) / 2;
+    }
 
-            public MinBinaryHeap(int capasity) {
-                buf = new int[capasity];
-            }
+    static private int left(int i) {
+        return 2 * i + 1;
+    }
 
-            private int parent(int i) {
-                return (i - 1) / 2;
-            }
+    static private int right(int i) {
+        return 2 * i + 2;
+    }
 
-            private int left(int i) {
-                return 2 * i + 1;
-            }
+    static private void heapify(int[] buf, int i, int size) {
+        int l = left(i);
+        int r = right(i);
 
-            private int right(int i) {
-                return 2 * i + 2;
-            }
+        int min = i;
 
-            public void add(int item) {
-                int i = size;
-                size += 1;
-
-                while (i != 0 && item < buf[parent(i)]) {
-                    buf[i] = buf[parent(i)];
-                    i = parent(i);
-                }
-
-                buf[i] = item;
-            }
-
-            private void restore(int i) {
-                int l = left(i);
-                int r = right(i);
-
-                int min = i;
-
-                if (l < size && buf[l] < buf[min]) {
-                    min = l;
-                }
-                if (r < size && buf[r] < buf[min]) {
-                    min = r;
-                }
-
-                if (min != i) {
-                    int temp = buf[min];
-                    buf[min] = buf[i];
-                    buf[i] = temp;
-                    restore(min);
-                }
-            }
-
-            public int remove() {
-                if (size == 1) {
-                    size--;
-                    return buf[0];
-                }
-
-                final int root = buf[0];
-
-                buf[0] = buf[size - 1];
-                size--;
-                restore(0);
-
-                return root;
-            }
+        if (l < size && buf[l] < buf[min]) {
+            min = l;
+        }
+        if (r < size && buf[r] < buf[min]) {
+            min = r;
         }
 
-        MinBinaryHeap heap = new MinBinaryHeap(array.length);
-        for (int x : array) {
-            heap.add(x);
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = heap.remove();
+        if (min != i) {
+            int temp = buf[min];
+            buf[min] = buf[i];
+            buf[i] = temp;
+            heapify(buf, min, size);
         }
     }
 
     /**
+     * Heapsort uisng min binary heap.
+     *
+     *
+     * @param array - Array that is being sorted. Mutates the passed array.
+     */
+    public static void heapsort(int[] array) {
+        int[] buf = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            int item = array[i];
+
+            while (i != 0 && item < buf[parent(i)]) {
+                buf[i] = buf[parent(i)];
+                i = parent(i);
+            }
+
+            buf[i] = item;
+        }
+
+        int size = array.length;
+        for (int i = 0; i < array.length - 1; i++) {
+            array[i] = buf[0];
+
+            buf[0] = buf[size - 1];
+            size--;
+            heapify(buf, 0, size);
+
+            size -= 1;
+        }
+
+        array[array.length - 1] = buf[0];
+    }
+
+    /**
      * Made to be able to run script.
-     * 
+     *
+     *
      * @param args - Main args.
      */
     public static void main(String[] args) {
