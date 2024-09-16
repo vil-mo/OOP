9aba4346541d85351bf81c7c32ed6df69415405d
vil'mo <morgraphvillain@gmail.com>
2024-09-16 19:20:52 +0700

    Temporarily removed all tests

--- Task_1_1_2/src/test/java/ru/nsu/berezin/Tests.java
+++ Task_1_1_2/src/test/java/ru/nsu/berezin/Tests.java
@@ -1,11 +1,8 @@
 package ru.nsu.berezin;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+class Tests {
+
 }
