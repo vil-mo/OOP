c304e7a0908286e1b6b687dff35d969e0bd81cc9
vil'mo <morgraphvillain@gmail.com>
2024-10-01 13:28:40 +0700

    Added number

--- Task_1_1_3/src/main/java/ru/nsu/berezin/expressions/Expression.java
+++ Task_1_1_3/src/main/java/ru/nsu/berezin/expressions/Expression.java
@@ -1,13 +1,41 @@
 package ru.nsu.berezin.expressions;
 
+/// Expression that represents an equation.
 public abstract class Expression {
+
+    /**
+     * Parses expression from string.
+     * 
+     * @param expression String to parse
+     * @return Null if string is not a valid expression.
+     */
     public static Expression parse(String expression) {
+        if (expression.matches("\\d+(\\.\\d+)?")) {
+            return new Number(Double.parseDouble(expression));
+        }
         return null;
     }
 
+    /**
+     * Returns string representation of expression.
+     * 
+     * @return String representation of expression
+     */
     public abstract String print();
 
+    /**
+     * Returns derivative of expression with respect to variable.
+     * 
+     * @param variable Variable to derivate with
+     * @return New expression
+     */
     public abstract Expression derivative(String variable);
 
+    /**
+     * Evaluates expression with variables denoting.
+     * 
+     * @param denoting String that has the form of "x = 1; y = 2"
+     * @return Value of expression
+     */
     public abstract double eval(String denoting);
 }
