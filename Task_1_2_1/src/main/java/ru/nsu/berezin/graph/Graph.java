b5be6f03071b422e15cdccf9a92d6cfdc03d62b4
vil'mo <morgraphvillain@gmail.com>
2024-10-15 15:11:46 +0700

    Added matrix graph

--- Task_1_2_1/src/main/java/ru/nsu/berezin/graph/Graph.java
+++ Task_1_2_1/src/main/java/ru/nsu/berezin/graph/Graph.java
@@ -13,15 +13,24 @@ import java.util.Optional;
  * @param <NodeIndex> Type of index for nodes
  * @param <EdgeIndex> Type of index for edges
  */
-public interface Graph<T, NodeIndex, EdgeIndex> {
+public interface Graph<T, NodeIndex> {
 
     /**
      * Adds a node to the graph.
      *
      * @param data Data to store in the node
      * @return Index of the new node
+     * @throws IllegalArgumentException if argument is null
      */
-    public NodeIndex addNode(T data);
+    public NodeIndex addNode(T data) throws IllegalArgumentException;
+
+    /**
+     * Returns data of a node.
+     *
+     * @param id Index of the node
+     * @return Data of the node. Empty if node was not found in the graph.
+     */
+    public Optional<T> getNode(NodeIndex id);
 
     /**
      * Removes a node from the graph.
@@ -41,20 +50,31 @@ public interface Graph<T, NodeIndex, EdgeIndex> {
     /**
      * Adds an edge between two nodes.
      *
-     * @param from Index of the node to which the edge will be added
-     * @param to Index of the node to which the edge will be added
-     * @return Index of the new edge
+     * @param from Edge from this node
+     * @param to Edge to this node
+     * @return False if one of the nodes was not found in graph.
      */
-    public EdgeIndex addEdge(NodeIndex from, NodeIndex to);
+    public boolean addEdge(NodeIndex from, NodeIndex to);
+
+    /**
+     * Returns true if the graph contains an edge between two nodes.
+     *
+     * @param from Edge from this node
+     * @param to Edge to this node
+     * @return True if the graph contains an edge between two nodes, false
+     * otherwise.
+     */
+    public boolean hasEdge(NodeIndex from, NodeIndex to);
 
     /**
      * Removes an edge from the graph.
      *
-     * @param id Index of the edge to remove
+     * @param from Edge from this node
+     * @param to Edge to this node
      * @return True if edge was removed, false if edge was not found in the
      * graph
      */
-    public boolean removeEdge(EdgeIndex id);
+    public boolean removeEdge(NodeIndex from, NodeIndex to);
 
     /**
      * Neighbors of a node.
@@ -63,14 +83,6 @@ public interface Graph<T, NodeIndex, EdgeIndex> {
      * @return List of neighbors of the node
      */
     public List<NodeIndex> neighbors(NodeIndex id);
-
-    /**
-     * Reads graph from a file.
-     *
-     * @param fileName Name of the file to read from
-     * @return Graph read from the file
-     */
-    public Graph<T, NodeIndex, EdgeIndex> fromFile(String fileName);
 
     private void topSortRec(NodeIndex node, HashSet<NodeIndex> visited, List<NodeIndex> result) {
         if (visited.contains(node)) {
--- Task_1_2_1/src/main/java/ru/nsu/berezin/graph/MatrixGraph.java
+++ Task_1_2_1/src/main/java/ru/nsu/berezin/graph/MatrixGraph.java
@@ -1,6 +1,138 @@
 package ru.nsu.berezin.graph;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 
-public class MatrixGraph {
+public class MatrixGraph<T extends Object> implements Graph<T, Integer> {
 
+    private final boolean[] edges;
+    private final Object[] nodes;
+
+    /**
+     * Creates a new matrix graph.
+     *
+     * @param maxNodesAmount Maximum amount of nodes in the graph
+     */
+    public MatrixGraph(int maxNodesAmount) {
+        edges = new boolean[maxNodesAmount * maxNodesAmount];
+        nodes = new Object[maxNodesAmount];
+    }
+
+    @Override
+    public Integer addNode(T data) throws IllegalArgumentException, IllegalStateException {
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null");
+        }
+        int id = 0;
+        for (Object o : nodes) {
+            if (o == null) {
+                break;
+            }
+            id++;
+        }
+        if (id == nodes.length) {
+            throw new IllegalStateException("Graph is full");
+        }
+
+        nodes[id] = data;
+        return id;
+    }
+
+    @Override
+    public Optional<T> getNode(Integer id) {
+        if (id < 0 || id >= nodes.length) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable((T) nodes[id]);
+    }
+
+    @Override
+    public Optional<T> removeNode(Integer id) {
+        if (id < 0 || id >= nodes.length) {
+            return Optional.empty();
+        }
+        T data = (T) nodes[id];
+        nodes[id] = null;
+        return Optional.ofNullable(data);
+    }
+
+    @Override
+    public Iterable<Integer> nodes() {
+        return () -> new Iterator<Integer>() {
+            int i = 0;
+
+            private void toNonNull() {
+                while (i < nodes.length && nodes[i] == null) {
+                    i++;
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                toNonNull();
+                return i < nodes.length;
+            }
+
+            @Override
+            public Integer next() {
+                toNonNull();
+                return i;
+            }
+        };
+    }
+
+    @Override
+    public boolean addEdge(Integer from, Integer to) {
+        if (from < 0 || from >= nodes.length) {
+            return false;
+        }
+        if (to < 0 || to >= nodes.length) {
+            return false;
+        }
+
+        edges[from * nodes.length + to] = true;
+        return true;
+    }
+
+    @Override
+    public boolean hasEdge(Integer from, Integer to) {
+        if (from < 0 || from >= nodes.length) {
+            return false;
+        }
+        if (to < 0 || to >= nodes.length) {
+            return false;
+        }
+
+        return edges[from * nodes.length + to];
+    }
+
+    @Override
+    public boolean removeEdge(Integer from, Integer to) {
+        if (from < 0 || from >= nodes.length) {
+            return false;
+        }
+        if (to < 0 || to >= nodes.length) {
+            return false;
+        }
+        boolean result = edges[from * nodes.length + to];
+        edges[from * nodes.length + to] = false;
+        return result;
+    }
+
+    @Override
+    public List<Integer> neighbors(Integer id) {
+        if (id < 0 || id >= nodes.length) {
+            return List.of();
+        }
+
+        List<Integer> result = new ArrayList();
+        for (int i = 0; i < nodes.length; i++) {
+            if (edges[id * nodes.length + i]) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
 }
