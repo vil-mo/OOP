18378febe13bf6677485f2afc0d5d26a58a03ccd
vil-mo <vil-mo@users.noreply.github.com>
2024-10-29 04:14:57 +0000

    Deploying to gh-pages from @ vil-mo/OOP@e327ecc3dc05ee442857c7f4a9d02d74059fc8f7 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.berezin.hashtable","l":"Bucket"},{"p":"ru.nsu.berezin.hashtable","l":"HashTable"}];updateSearchResults();
