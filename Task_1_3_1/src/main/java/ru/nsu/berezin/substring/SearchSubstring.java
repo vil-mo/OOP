4c881b40fa970379e134aab3b51414c4e4312574
vil'mo <morgraphvillain@gmail.com>
2024-11-11 21:37:43 +0700

    Initial commit

---
+++ Task_1_3_1/src/main/java/ru/nsu/berezin/substring/SearchSubstring.java
@@ -0,0 +1,26 @@
+package ru.nsu.berezin.substring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that hs function to search a substring in a string.
+ */
+public class SearchSubstring {
+
+    private SearchSubstring() {
+    }
+
+    /**
+     * Searches all occurrences of a substring in a file.
+     * 
+     * @param path - path to the file
+     * @param substr - substring to search
+     * @return list of all occurrences of the substring in the file
+     */
+    public static List<Long> search(String path, String substr) {
+        List<Long> result = new ArrayList<>();
+
+        return result;
+    }
+}
