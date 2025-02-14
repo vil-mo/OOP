3187e1fab90bc66b19e373c342bce92e8238d809
vil'mo <morgraphvillain@gmail.com>
2025-02-14 13:13:01 +0700

    Small benches

--- Task_2_1_1/src/jmh/java/ru/nsu/berezin/primes/PrimesBenchmark.java
+++ Task_2_1_1/src/jmh/java/ru/nsu/berezin/primes/PrimesBenchmark.java
@@ -1,6 +1,9 @@
 package ru.nsu.berezin.primes;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
+
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
@@ -11,4 +14,23 @@ import org.openjdk.jmh.infra.Blackhole;
 @Measurement(iterations = 20, time = 3, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
 public class PrimesBenchmark {
+
+    static int smallArray[] = {2, 3, 5, 7, 11, 13, 4};
+
+    @Benchmark
+    public static void smallSequential(Blackhole blackhole) {
+        Iterable<Integer> array = () -> Arrays.stream(smallArray).boxed().iterator();
+        blackhole.consume(AllPrimes.sequential(array));
+    }
+
+    @Benchmark
+    public static void smallThread(Blackhole blackhole) throws InterruptedException {
+        blackhole.consume(AllPrimes.threads(smallArray, 4));
+    }
+
+    @Benchmark
+    public static void smallParallelStream(Blackhole blackhole) {
+        Collection<Integer> array = Arrays.stream(smallArray).boxed().toList();
+        blackhole.consume(AllPrimes.parallelStream(array));
+    }
 }
