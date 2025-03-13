98e95dc1eac821681c924554f49080008fbd1c2a
vil'mo <morgraphvillain@gmail.com>
2025-03-13 21:23:10 +0700

    Initial commit

---
+++ Task_2_2_1/src/main/java/ru/nsu/berezin/pizzeria/Main.java
@@ -0,0 +1,18 @@
+package ru.nsu.berezin.pizzeria;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    private Main() {
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args - not used
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ Task_2_2_1/src/test/java/ru/nsu/berezin/pizzeria/Tests.java
@@ -0,0 +1,4 @@
+package ru.nsu.berezin.pizzeria;
+
+class Tests {
+}
